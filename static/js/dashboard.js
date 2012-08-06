1d85bd205f62eae66ab015821592ac4b6d0c95c9
Greg Reinbach <greg@reinbach.com>
2012-08-05 20:23:14 -0400

    Cleaned up dashboard js a little, now actually updates chart data dynamically. More cleanup needed not very dry at the moment

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -21,39 +21,60 @@ $(function() {
     // getting a list of meta data types
     socket.on('meta_data_types', function(data) {
         var d = $.parseJSON(data);
-        var meta_id = "meta-" + d.collection_id;
+        setUpChart(d);
+    });
+
+    // getting new data
+    socket.on('new_data', function(data) {
+        var d = $.parseJSON(data);
+        setUpChart(d);
+        updateChart(d.collection_id, d.data);
+    });
+
+    function setUpChart(meta) {
+        // if chart setup does not exist for the meta
+        // go ahead and create the setup
+        // and create the relevant charts with the data
+        // provided
+        var meta_id = "meta-" + meta.collection_id;
         if ($("#" + meta_id).length == 0) {
             $("#meta_data_types").append(
                 "<li><span class='label label-info' id='" +
-                    meta_id + "'>" + d.label + "</span></li>"
+                    meta_id + "'>" + meta.label + "</span></li>"
             );
 
             // setup charts/graphs
-	    var chart_id = "chart-" + d.collection_id;
-            var chart_div = "<div id='" + chart_id + "' class='chart-group'><h3>" + d.label + "</h3></div>";
+	    var chart_group_id = getChartGroupId(meta.collection_id);
+            var chart_div = "<div id='" + chart_group_id + "' class='chart-group'><h3>" + meta.label + "</h3></div>";
             $("#chart-container").append(chart_div);
 
       	    var width = 700;
-      	    var height = 300 / d.data.length;
-            for (i = 0; i < d.data.length; i++) {
-                var data = d.data[i];
-      	        createChart(data, chart_id, width, height);
+      	    var height = 300 / meta.data.length;
+            for (i = 0; i < meta.data.length; i++) {
+                var data = meta.data[i];
+      	        createChart(data, chart_group_id, width, height);
             }
 
             // toggle the display of graph for particular feed
             $("#" + meta_id).click(function() {
-                $("#" + chart_id).toggle();
+                $("#" + chart_group_id).toggle();
             });
 	}
-    });
-    function createChart(data, chart_id, width, height) {
+    }
+
+    function getChartGroupId(collection_id) {
+        return "chart-" + collection_id;
+    }
+
+    function createChart(data, chart_group_id, width, height) {
+        // create the relevant charts
       	var max = d3.max(data)
 
       	// scales
       	var x = d3.scale.linear().domain([0, data.length - 1]).range([0, width]);
       	var y = d3.scale.linear().domain([0, max]).range([height, 0]);
 
-      	var chart = d3.select("#" + chart_id).append("svg:svg")
+      	var chart = d3.select("#" + chart_group_id).append("svg:svg")
       	    .attr("class", "chart")
       	    .attr("width", width)
       	    .attr("height", height);
@@ -65,5 +86,42 @@ $(function() {
       	     	  .x(function(d, i) { return x(i); })
       	     	  .y(y)
       	     	 );
+    }
+
+    function updateChart(collection_id, data) {
+        // update the chart by adding new data
+        // and removing old data
+
+        var width = 700
+        var height = 300 / data.length;
+
+        //TODO handle the scenario when this is the new chart
+        var chart_group_id = getChartGroupId(collection_id);
+        var charts = $("#" + chart_group_id + " svg");
+        var paths = d3.selectAll("#" + chart_group_id + " path");
+        for (i = 0; i < data.length; i++) {
+            var chart = d3.select(charts[i]).select("path");
+            var chart_data = chart.data()[0]
+            // need to push the data onto the relevant data array
+            chart_data.push(data[i][0])
+
+            // scales
+      	    var max = d3.max(chart_data);
+      	    var x = d3.scale.linear().domain([0, chart_data.length - 1]).range([0, width]);
+            var y = d3.scale.linear().domain([0, max]).range([height, 0]);
+
+            // redraw line and slide to the left
+            chart
+                .attr("d", d3.svg.line()
+      	     	      .x(function(d, i) { return x(i); })
+      	     	      .y(y)
+                     )
+                .attr("transform", null)
+                .transition()
+                .ease("linear")
+                .attr("transform", "translate(" + x(-1) + ")");
+            // pop the old data point off the front
+            chart_data.shift()
+        }
     }
 });
